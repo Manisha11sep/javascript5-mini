4535136b18473171c523f51bbfa2529271e5e9c3
Manisha11sep <er.manisha11sep@gmail.com>
2018-04-10 16:42:10 -0700

    Added map function to display list of cars

--- app4/src/App.js
+++ app4/src/App.js
@@ -3,30 +3,40 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       cars: []
     }
+
+    this.getCars = this.getCars.bind(this);
   }
 
   getCars() {
-    axios.get('https://joes-autos.herokuapp.com/api/vehicles')
-    .then(res => {
-      this.setState({
-        cars: res
-      })
+    axios.get('https://joes-autos.herokuapp.com/api/vehicles').then(response => {
+      this.setState({cars: response.data});
+      console.log(this.state.cars);
     })
   }
 
   render() {
+const carList = this.state.cars.map((car,index)=>{
+  return(
+  <div key = {index}> 
+  <ul>{car.make} </ul>
+  <p>{car.model} </p>
+  <p>{car.year} </p>
+  <p>{car.color} </p>
+  </div>
+  )
+})
     return (
       <div className="App">
         <button onClick={this.getCars}>Get cars</button>
-        {this.state.cars}
+        {carList}
       </div>
-    );
+    )
   }
-}
 
+}
 export default App;
